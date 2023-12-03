e41c2c07a5fd3208ed6d942b32800cf5831051dd
Maria89698 <maria9parhomova@gmail.com>
2023-12-03 09:37:55 +0100

    робить 2 дз по React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
+import React from 'react';
+import { Greeting } from './Greeting';
+import { Message } from './Message';
+
+export function App() {
+  const name = 'Іван';
+  const message = 'Ласкаво просимо до нашого сайту!';
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Greeting name={name} />
+      <Message text={message} />
     </div>
   );
-};
+}
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export function Greeting({name}) {
+  return <h1>Привіт, {name}!</h1>;
+}
+
